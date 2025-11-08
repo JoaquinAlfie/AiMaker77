12d7b8989216b6ab08b27f8913a9245ce6150d26
Joaquin-Alfie <48178972@est.ort.edu.ar>
2025-11-08 17:05:08 -0300

    volver

--- src/components/chatbot.tsx
+++ src/components/chatbot.tsx
@@ -53,11 +53,14 @@ useEffect(() => {
 
 
   const handleNewChat = async () => {
-  await createChat("Nuevo Chat");   // crea el chat en backend
-  const allChats = await getAllChats();  // recarga todos
-  setChats(allChats.chats || allChats); //si no volver
-  const lastChat = (allChats.chats || allChats).slice(-1)[0]; // si no volver
-  if (lastChat) setActiveChat(lastChat.id); // si no volver
+    const res = await createChat("Nuevo Chat");
+    console.log("Nuevo chat creado:", res); // que guardo?
+    
+    if (!res.error) {
+      setChats([...chats, res]);
+      setActiveChat(res.id);
+      
+    }
   };
 
   const handleActiveChat = async (chatId: string) => {
