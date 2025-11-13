8daa309dd2e8fc6dba4f8aa84be8c350406b26f6
Joaquin-Alfie <48178972@est.ort.edu.ar>
2025-11-13 17:11:12 -0300

    probanding trans home

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -32,7 +32,9 @@ function Home({ user, setUser, setPage }: HomeProps) {
   return (
     <div className='casa'>
       <header className="navbar">
-        <img src="/img/Logox4.png" alt="Logo" className="logo" />
+        <a onClick={() => setPage("home")}>
+          <img src="/img/Logox4.png" alt="Logo" className="logo" />
+        </a>
         <section className="nav-items">
           <a className="support" onClick={() => setPage("support")}>
             <img
