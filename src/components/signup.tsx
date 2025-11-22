eb14b9b948c6d8c685e4709ed6244cfb6f4fd7e7
Valentin-Alfie <48178972@est.ort.edu.ar>
2025-11-22 18:18:30 -0300

    Pruebaaaa

--- src/components/signup.tsx
+++ src/components/signup.tsx
@@ -1,13 +1,13 @@
 import "../assets/styles/style-signup.css";
 import React, { useState } from "react";
-import { registerUser} from "../api/auth";
+import { registerUser, loginUser} from "../api/auth";
 
 interface SignupProps {
   setUser: React.Dispatch<React.SetStateAction<string>>;
   setPage: React.Dispatch<React.SetStateAction<"landing" | "signin" | "signup" | "home"| "chatbot" | "support">>;
 }
 
-function Signup({ setPage }: SignupProps) {
+function Signup({ setPage, setUser }: SignupProps) {
   const [name, setName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
@@ -37,11 +37,22 @@ function Signup({ setPage }: SignupProps) {
 
       console.log("Respuesta registro:", res);
 
-      if (res.code === "error" || res.error || res.message?.includes("Ya existe")){
-        setError(res.message || res.error);
+      if (res.code === "error"){
+        setError(res.message);
+        return;
+      }     const loginRes = await loginUser(email, password);
+    console.log("Respuesta login automático:", loginRes);
+
+    if (loginRes.token) {
+      localStorage.setItem("token", loginRes.token);
+      setUser(email);
+      setPage("home");
+      
+      
+      
       } else {
-        alert("Usuario creado con éxito. Ahora puedes iniciar sesión.");
-        setPage("signin");
+        setError(loginRes.message || loginRes.error || "No se pudo iniciar sesión automáticamente");
+      setPage("signin");
       }
     } catch {
       setError("Error al registrar usuario.");
